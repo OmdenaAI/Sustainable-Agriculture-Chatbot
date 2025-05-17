225d244a01bca530a030de9762baceb084a748e5
timhazed <thayes@oldzinsoftware.com>
2025-05-17 09:24:40 -0600

    Augment insert_db docker commands and add dockerfile that embeds transformer if mecessary. Add documentation diagrams to datapipeline

--- backend/tools/insert_db/docker_commands.sh
+++ backend/tools/insert_db/docker_commands.sh
@@ -1,10 +1,13 @@
 # No cache build
 docker build --no-cache -t insert-db .
 
+# Build it with sentence transformer preloaded
+docker build --no-cache -f Dockerfile_WithTransformer -t insert-db .
+
 # Example commands to run the container
 docker run \
   --env-file ~/src/python/.env_mini \
-  -v /Users/timhazed/tmp/output_test:/app/input \
+  -v /Users/timhazed/tmp/demo/output_demo/chunked:/app/input \
   -e PYTHONUNBUFFERED=1 \
   insert-db \
-  --input "/app/input/chunked/doc_0005"
+  --input "/app/input/doc_0002"
